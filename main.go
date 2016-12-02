11a354ab7f9f431eeb59c0304f4fa4a790c9a9f0
kokardy <mgc1028@gmail.com>
2016-12-02 12:24:40 +0900

    proxy setting

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "regexp"
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"regexp"
+)
 
 func Init() {
 
@@ -29,6 +34,29 @@ func Init() {
 	SAVE_DIR_Y = SETTINGS.SAVE_DIR_Y
 	SAVE_DIR_HOT = SETTINGS.SAVE_DIR_HOT
 
+	PROXY = SETTINGS.PROXY
+
+	if PROXY != "" {
+		if proxyURL, err := url.Parse(PROXY); err != nil {
+			fmt.Println("PROXY format must be 'scheme://[userinfo@]host/path[?query][#fragment]'")
+			fmt.Println(err)
+		} else {
+			ProxySetting := http.ProxyURL(proxyURL)
+			http.DefaultTransport = &http.Transport{
+				Proxy: ProxySetting,
+				/*
+					DialContext: (&net.Dialer{
+						Timeout:   30 * time.Second,
+						KeepAlive: 30 * time.Second,
+					}).DialContext,
+					MaxIdleConns:          100,
+					IdleConnTimeout:       90 * time.Second,
+					TLSHandshakeTimeout:   10 * time.Second,
+					ExpectContinueTimeout: 1 * time.Second,
+				*/
+			}
+		}
+	}
 }
 
 func main() {
