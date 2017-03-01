47ed6bf188f35858efc53c621714895a73853535
kokardy <2720900+kokardy@users.noreply.github.com>
2017-03-01 17:21:53 +0900

    under const.

--- docker/asset/medmst/asset/register.py
+++ docker/asset/medmst/asset/register.py
@@ -2,9 +2,31 @@
 
 import psycopg2 as pg
 import csv
+import os, re
+
+
+SAVE_DIR = "/bootstrap/save"
+
+def get_files(path, hot="hot", y="y"):
+    result = dict(hot=dict())
+    for path, dire, filename in os.walk(SAVE_DIR):
+        fullpath = os.path.join(path, filename)
+        if dire == hot:
+            r = result[hot]
+            if re.match("^MEDIS\d{8}.TXT$", filename):
+                r["main"] = fullpath
+            elif re.match("^\d{8}.txt", filename):
+                r["extra"] = r.get("extra", []).append(fullpath)
+            elif re.match("^h\d{8}del.txt", filename):
+                r["delete"] = fullpath
+        if dire == y:
+            if filename == "y.csv":
+                result[y] = fullpath
+
 
 def main():
-    pass
+    infiles = get_files(SAVE_DIR)
+    print(infiles)
 
 
 
