3caabb4da84fd07110d7b2085be09fcb2e21fe70
kokardy <2720900+kokardy@users.noreply.github.com>
2017-03-21 18:00:01 +0900

    under const.

--- docker/asset/medmst/asset/register.py
+++ docker/asset/medmst/asset/register.py
@@ -3,25 +3,26 @@
 import psycopg2 as pg
 import csv
 import os, re
-
+import os.path 
 
 SAVE_DIR = "/bootstrap/save"
 
 def get_files(path, hot="hot", y="y"):
     result = dict(hot=dict())
-    for path, dire, filename in os.walk(SAVE_DIR):
-        fullpath = os.path.join(path, filename)
-        if dire == hot:
-            r = result[hot]
-            if re.match("^MEDIS\d{8}.TXT$", filename):
-                r["main"] = fullpath
-            elif re.match("^\d{8}.txt", filename):
-                r["extra"] = r.get("extra", []).append(fullpath)
-            elif re.match("^h\d{8}del.txt", filename):
-                r["delete"] = fullpath
-        if dire == y:
-            if filename == "y.csv":
-                result[y] = fullpath
+    for path, dirs, filenames in os.walk(SAVE_DIR):
+        for filename in filenames:
+            fullpath = os.path.join(path, filename)
+            if os.path.basename(path) == hot:
+                r = result[hot]
+                if re.match("^MEDIS\d{8}.TXT$", filename):
+                    r["main"] = fullpath
+                elif re.match("^\d{8}.txt", filename):
+                    r["extra"] = r.get("extra", []).append(fullpath)
+                elif re.match("^h\d{8}del.txt", filename):
+                    r["delete"] = fullpath
+            if os.path.basename(path) == y:
+                if filename == "y.csv":
+                    result[y] = fullpath
 
 
 def main():
