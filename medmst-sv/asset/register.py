d97fce4de63c91f12ed4f1e565f1fbb95d826d86
Kota Kokado <kouta-k@mbm.nifty.com>
2019-02-06 18:54:11 +0900

    debug

--- medmst-sv/asset/register.py
+++ medmst-sv/asset/register.py
@@ -5,6 +5,9 @@ import csv
 import os, re
 import sys
 import os.path 
+import codecs
+
+ASSET_DIR = "/asset"
 
 if "MEDMST_SAVE" in os.environ:
     SAVE_DIR = os.environ["MEDMST_SAVE"]
@@ -28,12 +31,13 @@ def _connection(param):
 def get_files(save_dir=SAVE_DIR):
     hot = "hot"
     y = "y"
-    result = dict(hot=[], y=[])
+    medis = "medis"
+    result = dict(medis=[], y=[])
     for path, dirs, filenames in os.walk(save_dir):
         for filename in filenames:
             fullpath = os.path.join(path, filename)
             if os.path.basename(path) == hot:
-                r = result[hot]
+                r = result[medis]
                 if re.match("^MEDIS\d{8}.TXT$", filename):
                     r.append(fullpath)
                 elif re.match("^\d{8}.txt", filename):
@@ -44,24 +48,8 @@ def get_files(save_dir=SAVE_DIR):
                 if filename == "y.csv":
                     result[y] = [fullpath]
 
+    print("reuslt", result)
     return result
-
-def create(con):
-    filepath = os.path.join(SAVE_DIR, "medis_def.txt")
-    _sql_from_file(filepath)
-    cur = con.cursor()
-    try:
-        cur.execute(sql)
-    except Exception, e:
-        print e
-
-    filepath = os.path.join(SAVE_DIR, "y_def.txt")
-    _sql_from_file(filepath)
-    cur = con.cursor()
-    try:
-        cur.execute(sql)
-    except Exception, e:
-        print e
 
 def _sql_from_file(filepath):
     with codecs.open(filepath, "r", "utf8") as f:
@@ -69,17 +57,40 @@ def _sql_from_file(filepath):
         sql = "\n".join(lines)
     return sql
 
+def create(con):
+    filepath = os.path.join(ASSET_DIR, "medis_def.txt")
+    sql = _sql_from_file(filepath)
+    cur = con.cursor()
+    try:
+        cur.execute(sql)
+    except Exception, e:
+        print e
+
+    filepath = os.path.join(ASSET_DIR, "y_def.txt")
+    sql = _sql_from_file(filepath)
+    cur = con.cursor()
+    try:
+        cur.execute(sql)
+    except Exception, e:
+        print e
+
 def insert(con, infiles):
     infiles = get_files()
     
-    insert_list = [(os.path.join(SAVE_DIR, "{0}_insert.txt").format(table),
-        infiles[table], skip)
-            for (table, skip) in [("medis", True), ("y", False)]]
-    for (sql_file, insert_data, skip) in insert_list:
-        _insert(con, sql_file, insert_data, skip)
+    insert_list = []
+    for (table, skip) in [("medis", True), ("y", False)]:
+        sql_template = os.path.join(SAVE_DIR, "{0}_insert.txt").format(table)
+        insert_data =  infiles[table]
+        insert_list.append([sql_template, insert_data, skip])
+    for (sql_template, insert_data, skip) in insert_list:
+        _insert(con, sql_template, insert_data, skip)
 
 def _insert(con, sql_file, insert_files, line1skip):
-    sql = _sql_from_file(sql_file)
+    try:
+        sql = _sql_from_file(sql_file)
+    except IOError, e:
+        print(e)
+        return
     for insert_file in insert_files:
         with codecs.open(insert_file, "r", "utf-8") as f:
             r = csv.reader(f)
@@ -100,36 +111,41 @@ def delete(con):
 def C():
     con = connection()
     create(con)
+    con.commit()
 
 def I():
     con = connection()
     infiles = get_files()
     insert(con, infiles)
+    con.commit()
 
 def D():
     con = connection()
     delete(con)
+    con.commit()
 
 
 def main():
     infiles = get_files(SAVE_DIR)
 
     options = sys.argv[1].lstrip("-")
+
     
-    if len(options) > 0:
+    if len(options) == 0:
         print("OPTION must be -[C][D][I]")
         print("C: create table")
         print("D: delete table data")
         print("I: insert data to table")
         return
     
+    #OPTIONの分だけcreate delete insert関数登録
     exec_list = []
     if "C" in options:
         exec_list.append(C)
-    if "D" in options:
-        exec_list.append(D)
     if "I" in options:
         exec_list.append(I)
+    if "D" in options:
+        exec_list.append(D)
 
     for func in exec_list:
         func()
